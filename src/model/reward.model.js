2d43ed25bb7272d5bf1cae6befc8bd4ad434fcdf
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2024-11-06 19:02:46 +0530

    orderlist

---
+++ src/model/reward.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const rewardSchema = new mongoose.Schema({
+  rewardName: {
+    type: String,
+    required: true,
+  },
+  expiryDate: {
+    type: Date,
+    required: false,
+  },
+  requiredPointsToRedeemReward: {
+    type: Number,
+    required: true,
+  },
+  rewardCode: {
+    type: String,
+    required: false,
+  },
+  rewardDescription: {
+    type: String,
+    required: false,
+  },
+  redemptionLimit: {
+    type: Number,
+    required: false,
+  },
+  image: {
+    type: String, // This assumes a file URL or path. Adjust if needed.
+  
+  },
+  aboutOffer: {
+    type: [String], // Array to store multiple "About Offer" points
+    required: false,
+  },
+  termsOfUse: {
+    type: [String], // Array to store multiple "Terms of Use" points
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('Reward', rewardSchema);
