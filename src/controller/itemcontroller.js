4434409bd6e8a8cf1bb2e32c50d34b2f11306b69
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2024-11-26 12:58:14 +0530

    exports excel order list

--- src/controller/itemcontroller.js
+++ src/controller/itemcontroller.js
@@ -80,8 +80,8 @@ exports.createMenuItem = async (req, res) => {
             const transporter = nodemailer.createTransport({
                 service: 'gmail', // Use your email service
                 auth: {
-                    user: process.env.EMAIL_USER, // Your email address
-                    pass: process.env.EMAIL_PASS  // Your email password or app password
+                    user: process.env.EMAIL_USERNAME, // Your email address
+                    pass: process.env.EMAIL_PASSWORD  // Your email password or app password
                 }
             });
 
